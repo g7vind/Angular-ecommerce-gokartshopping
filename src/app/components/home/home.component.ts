132966e3315ac555b4c01283db0447e6bc05a9f2
g7vind <govindmh003@gmail.com>
2024-02-09 23:47:02 +0530

    updated pages

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,11 @@ import { FooterComponent } from '../../components/footer/footer.component';
 import { ProductComponent } from '../../pages/product/product.component';
 import { ContactComponent } from '../../pages/contact/contact.component';
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, ProductComponent, ContactComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, ProductComponent, ContactComponent,RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
